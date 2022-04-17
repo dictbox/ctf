4d3a39357ae29b358b8229b9470422643bc93748
wanlei <open@wanlei.net>
2022-04-17 22:17:37 +0800

    测试GUI Form Design

--- src/main/java/net/wanlei/ui/MainFrame.java
+++ src/main/java/net/wanlei/ui/MainFrame.java
@@ -4,6 +4,8 @@ import net.wanlei.data.dao.DbTools;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     final int DEFAULT_WIDTH = 800;
@@ -14,9 +16,18 @@ public class MainFrame extends JFrame {
 
         this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout(FlowLayout.LEFT,5,5));
-        panel.add(new TextButton("a","fabbbbb"));
-        panel.add(new TextButton("b","fdafdasfds"));
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        panel.add(new TextButton("a", "fabbbbb"));
+        panel.add(new TextButton("b", "fdafdasfds"));
+        JButton button = new JButton("+");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                (new TextEditorPane()).setVisible(true);
+            }
+        });
+
+        panel.add(button);
 
         this.add(panel);
     }
---
+++ src/main/java/net/wanlei/ui/TextEditorPane.java
@@ -0,0 +1,12 @@
+package net.wanlei.ui;
+
+import javax.swing.*;
+
+public class TextEditorPane extends JFrame {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+
+    public TextEditorPane() {
+        this.setSize(400,300);
+    }
+}
